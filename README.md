26b9b29209f366e18596606034fee6bcf22b863a
wesbin <leegosoo11@naver.com>
2024-05-29 01:43:05 +0900

    flutter first commit

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# 뭐먹(what-eat)
+# 뭐먹지(what-eat)
 점심과 저녁 메뉴를 선뜻 고르지 못하는 사람들을 위해 준비한 사이트입니다.
 
-감사합니다.
-
-https://wesbin.github.io/what-eat/
---
+++ android/app/src/main/kotlin/com/emptycastle/temp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.emptycastle.temp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
