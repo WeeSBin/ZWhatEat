fb7dd2da04ff03e35f6ba2397ae3f388d2021288
wesbin <leegosoo11@naver.com>
2024-05-31 01:10:45 +0900

    1. 카카오 api 테스트 완료

--- lib/store/store_screen.dart
+++ lib/store/store_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class StoreScreen extends StatefulWidget {
   const StoreScreen({super.key});
@@ -7,9 +8,37 @@ class StoreScreen extends StatefulWidget {
   State<StoreScreen> createState() => _StoreScreenState();
 }
 
+Future<void> fetchPlaces() async {
+  // todo env 도입
+  // todo Geolocator 위도 경도 확인
+  const String apiKey = '';
+  var headers = {
+    'Authorization': 'KakaoAK $apiKey'
+  };
+  var url = Uri.parse('https://dapi.kakao.com/v2/local/search/category.json?category_group_code=PM9&radius=20000');
+
+  try {
+    var response = await http.get(url, headers: headers);
+    if (response.statusCode == 200) {
+      print('API Response: ${response.body}');
+    } else {
+      print('Failed to load data. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Caught exception: $e');
+  }
+}
+
 class _StoreScreenState extends State<StoreScreen> {
+
   @override
   Widget build(BuildContext context) {
-    return const Text("StoreScreen");
+    return ElevatedButton(
+      onPressed: () {
+        print("Click!!!");
+        fetchPlaces();
+      },
+      child: const Text("BUTTON"),
+    );
   }
 }
